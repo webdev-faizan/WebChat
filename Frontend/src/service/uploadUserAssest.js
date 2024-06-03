8844203bf504ee294c9131f9de69ff652f796b40
webdev-faizan <webdev.faizanali@gmail.com>
2024-06-03 09:09:18 -0400

    Utilized environment variables 🌟

--- Frontend/src/service/uploadUserAssest.js
+++ Frontend/src/service/uploadUserAssest.js
@@ -2,10 +2,11 @@ export const uploadUserAssest = async (image) => {
   if (!image.type.startsWith("image/")) return;
   let formData = new FormData();
   formData.append("file", image);
-  formData.append("cloud_name",process.env.REACT_APP_CLOUD_NAME);
+  return
+  formData.append("cloud_name", process.env.REACT_APP_CLOUD_NAME);
   formData.append("upload_preset", process.env.REACT_APP_UPLOAD_PRESENT);
   const response = await fetch(
-    "https://api.cloudinary.com/v1_1/dkhgfsefj/upload",
+    `https://api.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUD_NAME}/upload`,
     {
       method: "POST",
       body: formData,
